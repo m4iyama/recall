3b558c75f946da894bf931f8ea544d164343692e
m4iyama <myam0118@gmail.com>
2018-06-04 23:00:57 +0900

    add username to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :confirmable
+
+  validates :username, presence: true, uniqueness: { case_sensitive: false }
 end
